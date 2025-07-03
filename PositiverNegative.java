e52495ccff3558637cf3776cba8466fe2312f52e
Venkata Pradeep Devarampati <165290628+VenkataPradeep43@users.noreply.github.com>
2025-07-03 10:43:55 +0530

    Add files via upload

---
+++ PositiverNegative.java
@@ -0,0 +1,11 @@
+class PositiverNegative {
+    public static void main(String[] args) {
+      int n=0;
+      if(n>0) System.out.print("Positive");
+      else if(n<0) System.out.print("Negative");
+      else System.out.print("Zero");
+    }
+}
+
+// Output:
+// Zero
