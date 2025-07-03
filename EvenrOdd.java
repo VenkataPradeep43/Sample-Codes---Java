d4dcc840650a06024595cdbd1ba81f2f7c494372
Venkata Pradeep Devarampati <165290628+VenkataPradeep43@users.noreply.github.com>
2025-07-03 10:47:25 +0530

    Add files via upload

---
+++ EvenrOdd.java
@@ -0,0 +1,11 @@
+
+class EvenrOdd {
+    public static void main(String[] args) {
+      int n=7;
+      if(n%2 == 0) System.out.print("Even");
+      else System.out.print("Odd");
+    }
+}
+
+// Output:
+// Odd
