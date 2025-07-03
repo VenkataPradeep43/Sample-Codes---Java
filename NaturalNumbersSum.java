b619dfc4abc2578b2e4589c65e2f96f14d377f39
Venkata Pradeep Devarampati <165290628+VenkataPradeep43@users.noreply.github.com>
2025-07-03 10:53:25 +0530

    Add files via upload

---
+++ NaturalNumbersSum.java
@@ -0,0 +1,13 @@
+
+class NaturalNumbersSum {
+    public static void main(String[] args) {
+      int n=5;
+      int sum=0;
+      for(int i=1;i<=n;i++)
+          sum+=i;
+      System.out.print(sum);
+    }
+}
+
+// Output:
+// 15
