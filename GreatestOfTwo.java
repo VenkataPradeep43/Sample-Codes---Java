c1d3f5cc22b7b0fd52ca520cc9b43dd727faafe2
Venkata Pradeep Devarampati <165290628+VenkataPradeep43@users.noreply.github.com>
2025-07-08 14:01:44 +0530

    Add files via upload

---
+++ GreatestOfTwo.java
@@ -0,0 +1,14 @@
+
+class GreatestOfTwo {
+    public static void main(String[] args) {
+        int a=13;
+        int b=27;
+        System.out.println("A = "+a+" "+"B = "+b);
+        if(a>b) System.out.println("A is Greater");
+        else System.out.println("B is Greater");
+    }
+}
+
+// Output:
+// A = 13 B = 27
+// B is Greater
